d5003f1a7b24f426697846697d13315f939bdc82
Christopher Vestman <christophervestman@gmail.com>
2025-06-05 13:54:22 +0200

    feat: CH03, L02

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import get_num_words, get_chars_dict, chars_dict_to_sorted_list
+import sys
 
 
 def get_book_text(filepath):
@@ -23,11 +24,16 @@ def print_report(book_path, num_words, chars_sorted_list):
 
 def main():
     book_path = "books/frankenstein.txt"
-    text = get_book_text(book_path)
-    num_words = get_num_words(text)
-    chars_dict = get_chars_dict(text)
-    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
-    print_report(book_path, num_words, chars_sorted_list)
+    if len(sys.argv) < 2:
+        print(f"Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        book_path = sys.argv[1]
+        text = get_book_text(book_path)
+        num_words = get_num_words(text)
+        chars_dict = get_chars_dict(text)
+        chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+        print_report(book_path, num_words, chars_sorted_list)
 
 
 main()
